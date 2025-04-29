9f515e9bf15419724034b6a3b75b490499732a3b
Bartosz <par-gluten-0f@icloud.com>
2025-04-29 15:45:27 +0200

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Bartosz Mrugała on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
